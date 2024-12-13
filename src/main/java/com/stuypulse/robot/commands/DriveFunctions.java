f584b4c45b6eb26067812529fe56864c562e6f3c
Evan Khosh <evank43@nycstudents.net>
2024-12-13 14:24:03 -0500

    Copied over drive functions done on old MotorLab

--- src/main/java/com/stuypulse/robot/commands/DriveFunctions.java
+++ src/main/java/com/stuypulse/robot/commands/DriveFunctions.java
@@ -4,21 +4,53 @@ import com.stuypulse.robot.util.Motor;
 
 public class DriveFunctions {
     
-    public static void driveForwards(Motor left, Motor right) {}
+    public static void driveForwards(Motor left, Motor right) {
+        left.set(1.0);
+        right.set(1.0);
+    }
 
-    public static void driveBackwards(Motor left, Motor right) {}
+    public static void driveBackwards(Motor left, Motor right) {
+        left.set(-1.0);
+        right.set(-1.0);
+    }
 
-    public static void turnRight(Motor left, Motor right) {}
+    public static void stop(Motor left, Motor right) {
+        left.set(0.0);
+        right.set(0.0);
+    }
 
-    public static void turnLeft(Motor left, Motor right) {}
+    public static void turnRight(Motor left, Motor right) {
+        left.set(1.0);
+        right.set(-1.0);
+    }
+
+    public static void turnLeft(Motor left, Motor right) {
+        left.set(-1.0);
+        right.set(1.0);
+    }
 
     public static void arcRight(Motor left, Motor right) {}
 
     public static void arcLeft(Motor left, Motor right) {}
 
-    public static void stopDistance(Motor left, Motor right) {}
+    public static void stopDistance(Motor left, Motor right) {
+        double stopPoint = 60.0;
+        if (right.getDistance() < stopPoint){
+            driveForwards(left, right);
+        } else {
+            stop(left, right);
+        }
+    }
 
-    public static void bangBang(Motor left, Motor right) {}
+    public static void bangBang(Motor left, Motor right) {
+        double stopPoint = 60.0;
+        if (right.getDistance() < stopPoint){
+            driveForwards(left, right);
+        }
+        if (right.getDistance() > stopPoint){
+            driveBackwards(left, right);
+        }
+    }
 
     public static void lessBang(Motor left, Motor right) {}
 
